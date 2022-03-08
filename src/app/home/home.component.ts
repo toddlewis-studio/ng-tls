5b0315028a0ef95e84f9471e5a78201e8ed5e8cb
Todd Lewis aka Brayan <brayanbyrdsong@icloud.com>
2022-03-08 11:21:32 -0700

    style changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,14 +26,7 @@ export class HomeComponent implements OnInit {
       './assets/wisp-pv.PNG',
       'Collect Tokens and assemble wisps. Use a team of wisps to battle your friends and enemies.',
       '*Plans for NFT opensea integration.',
-    ),
-    this.toCard(
-      'i0 Framework',
-      'https://www.i0framework.com',
-      'Javascript Framework',
-      'https://i04--blbbrayan.repl.co/www/assets/logo-ico.png',
-      './assets/i0-pv.png',
-      'Use i0 to quickly create websites. i0 handles frontend and backend and also has Firebase database integration.'
+      'Created with i0, NodeJS, and Firebase.'
     ),
     this.toCard(
       'Essencials',
@@ -42,7 +35,8 @@ export class HomeComponent implements OnInit {
       'https://www.essencials.page/assets/logo.PNG',
       './assets/essencials-pv.PNG',
       'Daily tarot card reading and wellness exercises. Daily exercises are one of each category: affirmation, chakra, meditation, yoga.',
-      'Track your own affirmations, goals, budgets, and your schedule.'
+      'Track your own affirmations, goals, budgets, and your schedule.',
+      'Created with i0 and NodeJS.'
     ),
     this.toCard(
       'Gamepro Studio',
@@ -51,7 +45,17 @@ export class HomeComponent implements OnInit {
       'https://www.gamepro.studio/assets/logo.png',
       './assets/gamepro-pv.PNG',
       'Create games with javascript that export as a bundle of assets, html, js, and css files.',
-      'Styled for IPad browser "Inspect" which allows for inspecting and other javascript developer tools.'
+      'Styled for IPad browser "Inspect" which allows for inspecting and other javascript developer tools.',
+      'Created with ES6+ and NodeJS.'
+    ),
+    this.toCard(
+      'i0 Framework',
+      'https://www.i0framework.com',
+      'Javascript Framework',
+      'https://i04--blbbrayan.repl.co/www/assets/logo-ico.png',
+      './assets/i0-pv.png',
+      'Use i0 to quickly create websites. i0 handles frontend and backend and also has Firebase database integration.',
+      'Created with ES6+ and NodeJS.'
     )
   ]
 
